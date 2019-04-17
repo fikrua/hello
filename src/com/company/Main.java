60355cd0ec9e50cfc8c08df3bc5906341b69b422
fikrua <fikruayal@gmail.com>
2019-04-17 10:29:28 -0400

    hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the name ");
+        name = scanner.nextLine();
+        System.out.println("hello " + name + "!");
+        System.out.println("today is april 17, 2019. i am  alive!");
+	// write your code here
+    }
+}
